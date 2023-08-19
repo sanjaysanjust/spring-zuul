6486501d49976f81b7848e5673a83ceca873a429
Sanjay <sanjay.s.t@kyndryl.com>
2023-08-19 14:03:58 +0530

    First commit

---
+++ src/main/java/com/sanjay/zuul/SpringZuulApplication.java
@@ -0,0 +1,15 @@
+package com.sanjay.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class SpringZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZuulApplication.class, args);
+	}
+
+}
